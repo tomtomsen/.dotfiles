8d1031ca076bd475c89c111a4f36332e546d1668
tom tomsen <tom.tomsen@inbox.com>
2016-03-04 22:11:25 +0100

    updated install.sh. based on current holman/dotfiles

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # defines
-DOTFILES_ROOT="`pwd`"
+cd "$(dirname "$0")"
+DOTFILES_ROOT=$(pwd -P)
 
-# functions
+set -e
+
+echo ''
+
 info () {
-   printf "\r  [ \033[00;34m..\033[0m ] $1"
+   printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
 
 user () {
-   printf "\r  [ \033[0;33m?\033[0m ] $1 "
+   printf "\r  [ \033[0;33m?\033[0m ] $1\n"
 }
 
 success () {
@@ -34,40 +38,32 @@ run_install_scripts() {
 
     success "everything has been installed"
 }
-run_install_scripts
+#run_install_scripts
 
-install_dotfiles () {
-	echo ''
-	info 'installing dotfiles'
+link_file() {
+	local src=$1 dst=$2
 
-	overwrite_all=false
-	backup_all=false
-	skip_all=false
+	local overwrite= backup= skip=
+	local action=
 
-	echo "find $DOTFILES_ROOT -maxdepth 2 -name \".symlink"
+	success "$dst"
 
-	for source in `find $DOTFILES_ROOT -maxdepth 2 -name \*.symlink`
-	do
-		dest="$HOME/.`basename \"${source%.*}\"`"
+	if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+	then
 
-		success "$dest"
-
-		if [ -f $dest ] || [ -d $dest ]
+		if [ "$overwrite_all" == "false" -a "$backup_all" == "false" -a "$skip_all" == "false" ]
 		then
-			overwrite=false
-			backup=false
-			skip=false
 
-			if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+			local currentSrc="$(readlink $dst)"
+			if [ "$currentSrc" == "$src" ]
 			then
-				user "File already exists: `basename $dest`, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+				skip=true;
+			else
 
+				user "File already exists: $dst ($(basename "$src")), what do you want to do?\n[s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
 				read -n 1 action
+
 				case "$action" in
-					s )
-						skip=true;;
-					S )
-						skip_all=true;;
 					o )
 						overwrite=true;;
 					O )
@@ -76,34 +72,56 @@ install_dotfiles () {
 						backup=true;;
 					B )
 						backup_all=true;;
+					s )
+						skip=true;;
+					S )
+						skip_all=true;;
 					* )
 						;;
 				esac
+
 			fi
 
-			if [ "$overwrite" == "true" ] || [ "$overwrite_all" == "true" ]
-			then
-				rm -rf $dest
-				success "removed $dest"
-			fi
-
-			if [ "$backup" == "true" ] || [ "$backup_all" == "true" ]
-			then
-				mv $dest $dest\.backup
-				success "moved $dest to $dest.backup"
-			fi
-
-			if [ "$skip" == "false" ] && [ "$skip_all" == "false" ]
-			then
-				ln -s $source $dest
-				success "linked $source to $dest"
-			else
-				success "skipped $source"
-			fi
-		else
-			ln -s $source $dest
-			success "linked $source to $dest"
 		fi
+
+		overwrite=${overwrite:-$overwrite_all}
+		backup=${backup:-$backup_all}
+		skip=${skip:-$skip_all}
+
+		if [ "$overwrite" == "true" ]
+		then
+			rm -rf "$dst"
+			success "removed $dst"
+		fi
+
+		if [ "$backup" == "true" ]
+		then
+			mv "$dst" "${dst}.backup"
+			success "moved $dst to ${dst}.backup"
+		fi
+
+		if [ "$skip" == "true" ]
+		then
+			success "skipped $src"
+		fi
+	fi
+
+	if [ "$skip" != "true" ]  # "false" or empty
+	then
+		ln -s "$1" "$2"
+		success "linked $1 to $2"
+	fi
+}
+
+install_dotfiles () {
+	info 'installing dotfiles'
+
+	local overwrite_all=false backup_all=false skip_all=false
+
+	for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink')
+	do
+		dst="$HOME/.$(basename "${src%.*}")"
+		link_file "$src" "$dst"
 	done
 }
 
